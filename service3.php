80bfb450c2b7adac84ea9711d0c9bd54a96134e0
Nick Carlos Mesias Rivera <nickmeza@upeu.edu.pe>
2020-05-24 12:30:28 -0500

    nick

--- service3.php
+++ service3.php
@@ -3,8 +3,7 @@ include('lib/nusoap.php');
 $client = new nusoap_client('https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl','wsdl');
 $err = $client->getError();
 if ($err) {	echo 'Error en Constructor' . $err ; }
-$param = array('plengua' => 'C','pcurso'=> '2011-12','pcodcen'=>'B142','pcodest'=>'');
-$result = $client->call('wasisdepto', $param);
-print_r($result);
+$param = array('plengua' => 'C','pcurso'=> '2011-12','pcoddep'=>'B142','pcodest'=>'');
+$result1 = $client->call('wsasidepto', $param);
+print_r($result1);
 ?>
-
